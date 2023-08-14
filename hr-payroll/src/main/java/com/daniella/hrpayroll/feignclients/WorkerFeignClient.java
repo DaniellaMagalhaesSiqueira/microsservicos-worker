272bbf5f8d9c647fc6389e83d68fb536a59d792a
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-08-14 19:45:39 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/daniella/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/daniella/hrpayroll/feignclients/WorkerFeignClient.java
@@ -10,7 +10,8 @@ import com.daniella.hrpayroll.entities.Worker;
 
 
 @Component
-@FeignClient(name="hr-worker", url="localhost:8001", path="/workers")
+//@FeignClient(name="hr-worker", url="localhost:8001", path="/workers")
+@FeignClient(name="hr-worker", path="/workers")
 public interface WorkerFeignClient {
 	
 	@GetMapping(value="/{id}")
